b607b2217bd56d75bbee462443bfd24ed8c8d547
bonnie-mcconnell <bonniep.mcconnell@gmail.com>
2026-08-29 23:20:20 +1200

    update cors and index to point to deployed render static site

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],
+    allow_origins=["http://localhost:5500", "https://promptops-frontend.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
